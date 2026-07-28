c57aa4cf8223c0ae193d76cf43411f633e07f77c
MUhammad Imran Butt <imrancodes0546@gmail.com>
2026-07-28 13:44:26 +0500

    feat: resolve error__

--- main.py
+++ main.py
@@ -239,8 +239,9 @@ _init_db_sync()
 
 
 if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
     mcp.run(
         transport="http",
         host="0.0.0.0",
-        port=8000,
+        port=port,
     )
